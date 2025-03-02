0d64840e9ed2ba3f29cb88c9290f367b198d51bd
evangeliopleno <evangeliopleno@gmail.com>
2025-03-02 01:45:01 -0500

    Updated 03-02 01:44

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3426945758"
+    serviceWorkerVersion: "2806469908"
   }
 });
